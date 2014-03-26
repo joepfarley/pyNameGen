c1b51f020b43e063f42c1a58e6c8f6ad342a330e
Joseph Farley <admin@joepfarley.com>
2014-03-25 21:46:21 -0500

    Swapped tabs for spaces to be in style compliance.

--- nameGen.py
+++ nameGen.py
@@ -40,7 +40,7 @@ print("""<style type="text/css"><!--/*--><![CDATA[/*><!--*/
     ol {
     font-family:monospace;
     font-size:16;
-        }
+    }
 /*]]>*/--></style>""")
 
 print
@@ -98,103 +98,103 @@ syls1[1]+syls1[0]+syls1[2]
 ]
 syls=syls1+syls2
 def nameGen(*args):
-	args=args[0]
-	name=[]
-	result=""
-	for i in args:
-		try:
-			time.sleep(.0001)
-			name.append(choice(syls[int(i)-1]))
-		except ValueError:
-			name.append(i)
-		
-	result=("".join(name).title())
-	return(result)
+    args=args[0]
+    name=[]
+    result=""
+    for i in args:
+        try:
+            time.sleep(.0001)
+            name.append(choice(syls[int(i)-1]))
+        except ValueError:
+            name.append(i)
+        
+    result=("".join(name).title())
+    return(result)
 
 def frontEnd():
-	print("""
-	<title>Name Generator</title>
-	<h1>Name generator</h1>
-	<p>
-	To use this name generator reference the lest below and place the number in the text box for the type of syllable you want to use. The first number refers to the number of names that will be generated. Please keep the number to 300 or lower. You may place your own string into any box or simply leave the box blank if you don't want to use it. 
-	</p>
-	<form>
-	<ul>
-	<li>Number of names:<input type="text" id="num" size="3" value="15"></li>
+    print("""
+    <title>Name Generator</title>
+    <h1>Name generator</h1>
+    <p>
+    To use this name generator reference the lest below and place the number in the text box for the type of syllable you want to use. The first number refers to the number of names that will be generated. Please keep the number to 300 or lower. You may place your own string into any box or simply leave the box blank if you don't want to use it. 
+    </p>
+    <form>
+    <ul>
+    <li>Number of names:<input type="text" id="num" size="3" value="15"></li>
     <li>Syllable one:<input type="text" id="syl1" size="3"value="6"></li>
-	<li>Syllable two:<input type="text" id="syl2" size="3"value="7"></li>
-	<li>Syllable three:<input type="text" id="syl3" size="3"value=" "></li>
-	<li>Syllable four:<input type="text" id="syl4" size="3"value="16"></li>
-	<li>Syllable five:<input type="text" id="syl5" size="3"value="14"></li>
-	<li>Syllable six:<input type="text" id="syl6" size="3"value=""></li>
-	</br><a href='javascript:var num = document.getElementById("num").value;var syl1 = document.getElementById("syl1").value;var syl2 = document.getElementById("syl2").value;var syl3 = document.getElementById("syl3").value;var syl4 = document.getElementById("syl4").value;var syl5 = document.getElementById("syl5").value;var syl6 = document.getElementById("syl6").value;location = ( "?" + num + "+" + syl1 + "+" + syl2 + "+" + syl3 + "+" + syl4 + "+" + syl5 + "+" + syl6);'> Generate List </a>
-	</ul>
+    <li>Syllable two:<input type="text" id="syl2" size="3"value="7"></li>
+    <li>Syllable three:<input type="text" id="syl3" size="3"value=" "></li>
+    <li>Syllable four:<input type="text" id="syl4" size="3"value="16"></li>
+    <li>Syllable five:<input type="text" id="syl5" size="3"value="14"></li>
+    <li>Syllable six:<input type="text" id="syl6" size="3"value=""></li>
+    </br><a href='javascript:var num = document.getElementById("num").value;var syl1 = document.getElementById("syl1").value;var syl2 = document.getElementById("syl2").value;var syl3 = document.getElementById("syl3").value;var syl4 = document.getElementById("syl4").value;var syl5 = document.getElementById("syl5").value;var syl6 = document.getElementById("syl6").value;location = ( "?" + num + "+" + syl1 + "+" + syl2 + "+" + syl3 + "+" + syl4 + "+" + syl5 + "+" + syl6);'> Generate List </a>
+    </ul>
     </form><ol>
-	<h2> Syllable List </h2>
-	""")
-	
-	for i in syls:
-		print("<li>")
-		for i2 in sample(i,5):
-			print("{0:^0} ".format(i2))
-		print("</li>")
-	print("</ol>")
-		
+    <h2> Syllable List </h2>
+    """)
+    
+    for i in syls:
+        print("<li>")
+        for i2 in sample(i,5):
+            print("{0:^0} ".format(i2))
+        print("</li>")
+    print("</ol>")
+        
 def helper():
-	print("""
-	<title>Advanced help</title>
-	<h1>Advanced Help</h1>
-	<p>
-	Right now this application uses interger arguments to define the name types. 
-	The URL should look something like example.com/name.py?A+B+C+D+E+F
-	Where each letter is an integer. Anything other than an integer will cause an error.
-	</p></br>
-	<ul>
-	<li>A) indicates the number of names generated. please limit number to a value no greater than 300.</li>
-	<li> All numbers after A are arbitrary numbers starting at 1. Each number is an index key to a list of lists. Each list is a set of letter combinations. </li>
-	<li><a href='/cgi/name.py?frontEnd'> Front End </a></li>
-	</ul>
-	<h2>Syllable Lists</h2>
-	<ol>
-	""")
+    print("""
+    <title>Advanced help</title>
+    <h1>Advanced Help</h1>
+    <p>
+    Right now this application uses interger arguments to define the name types. 
+    The URL should look something like example.com/name.py?A+B+C+D+E+F
+    Where each letter is an integer. Anything other than an integer will cause an error.
+    </p></br>
+    <ul>
+    <li>A) indicates the number of names generated. please limit number to a value no greater than 300.</li>
+    <li> All numbers after A are arbitrary numbers starting at 1. Each number is an index key to a list of lists. Each list is a set of letter combinations. </li>
+    <li><a href='/cgi/name.py?frontEnd'> Front End </a></li>
+    </ul>
+    <h2>Syllable Lists</h2>
+    <ol>
+    """)
 
-	for i in syls:
-		print("<li>")
-		for i2 in i:
-			print("{0:^0} ".format(i2))
-		print("</li>")
-	
-	print("""
-	<p><ul>
-	Click on the links below for various examples.
-	<li><a href='/cgi/name.py?15+6+7'>Example 1 </a> </li>
-	<li><a href='/cgi/name.py?15+2+8+10'>Example 2</a> </li>
+    for i in syls:
+        print("<li>")
+        for i2 in i:
+            print("{0:^0} ".format(i2))
+        print("</li>")
+    
+    print("""
+    <p><ul>
+    Click on the links below for various examples.
+    <li><a href='/cgi/name.py?15+6+7'>Example 1 </a> </li>
+    <li><a href='/cgi/name.py?15+2+8+10'>Example 2</a> </li>
     <li><a href='/cgi/name.py?15+3+4+14+3'>Example 3</a> </li>   
     <li><a href='/cgi/name.py?15+8+14+14'>Example 4</a> </li>
     <li><a href='/cgi/name.py?15+16+14+12+14+3'>Example 5</a> </li>
     <li><a href='/cgi/name.py?15+6+6+10+%20+14+14'>Example 6</a> </li>
     <li><a href='/cgi/name.py?15+10+9'>Example 7</a> </li>
-	</ul></p>
-	""")
+    </ul></p>
+    """)
 
 try:
-	if sys.argv[1] == 'help':
-		helper()
+    if sys.argv[1] == 'help':
+        helper()
 except:pass
 
 li=[]
 
 try:
-	num=int(sys.argv[1])
+    num=int(sys.argv[1])
 except IndexError:pass
 except ValueError:frontEnd()
 
 try:
-	if num>300:
-		print("<sub>Don't make more names than 300. Limiting to 300 names.</sub>")
-		num=300
-	for i in range(0,num):
-		li.append(nameGen(sys.argv[2:]))
+    if num>300:
+        print("<sub>Don't make more names than 300. Limiting to 300 names.</sub>")
+        num=300
+    for i in range(0,num):
+        li.append(nameGen(sys.argv[2:]))
 except ValueError:pass
 except NameError:pass
 except IndexError:frontEnd()
@@ -204,8 +204,8 @@ li.sort()
 
 print('<ul>')
 for i in li:
-	if i==li[0]:print("<title>{0:^0}</title>".format(choice(li)))
-	print("<li>{0:^0}</li>".format(i))
+    if i==li[0]:print("<title>{0:^0}</title>".format(choice(li)))
+    print("<li>{0:^0}</li>".format(i))
 
 print("""</br><sub>
 <a href="javascript:location.reload(true);"> Reload list </a></br>
